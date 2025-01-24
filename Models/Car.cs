fe4c061f270dd6c35eaa5e244034bc3e14b60d11
BarKoc02 <bartekkocik369@gmail.com>
2025-01-24 22:51:44 +0100

    Add files via upload

---
+++ Models/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace LuxCars.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Engine { get; set; }
+        public required string Color { get; set; }
+        public required decimal Price_per_day {  get; set; }
+
+    }
+}
